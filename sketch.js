4ac3be83a8bb4249af44a1cef5938ff554ddd545
ava4v4 <avawadley122@gmail.com>
2024-05-23 17:37:16 -0500

    mega push
    added works, design tabs and edited landing page p5js. added work images and design images.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
 const s = ( sketch ) => {
 
 let x, y; 
-let xspeed = 50;
-let yspeed = 16; 
+let xspeed = 0;
+let yspeed = 6; 
 let xdirection = 1; 
 let ydirection = 1; 
-let d = 10; 
+let d = 100; 
 let count = 100; 
+let redcount = 240; 
+let bluecount = 100; 
+let greencount = 100; 
+let honeydew = sketch.color(58,197,224);
+let rosemist = sketch.color(188,218,238);
+let seashell = sketch.color(255,246,252);
+let honeyToRose = true;
+let roseToSeashell = false;
+let seashellToHoney = false;
+let gradcount = 0;
+let fillcolor = honeydew;
+let incr = .5;
 
 sketch.setup = () => {
-  sketch.createCanvas(sketch.windowWidth, 55);
-  x = sketch.width/4;
-  y = sketch.height/4;
-  w = sketch.width + 20;
+  sketch.createCanvas(sketch.windowWidth, sketch.windowHeight - 100);
+  x = sketch.width/2;
+  y = sketch.height/2;
+  w = sketch.width + 200;
   dx = (sketch.TWO_PI / period) * xspacing;
   yvalues = new Array(sketch.floor(w / xspacing));
   sketch.clear();
 }
-
+//240,255,240
+//255,228,225
+//255,245,238
 sketch.draw = () => {
   calcWave();
   renderWave();
@@ -44,12 +58,12 @@ sketch.draw = () => {
 }
 
 
-let xspacing = 34; // Distance between each horizontal location
-let w; // Width of entire wave
-let theta = 0.5; // Start angle at 0
-let amplitude = 16.0; // Height of wave
-let period = 50.0; // How many pixels before the wave repeats
-let dx; // Value for incrementing x
+let xspacing = 40; // Distance between each horizontal location
+let w = 600; // Width of entire wave
+let theta = 1; // Start angle at 0
+let amplitude = sketch.windowHeight - 100; // Height of wave
+let period = 100.0; // How many pixels before the wave repeats
+let dx = 12.0; // Value for incrementing x
 let yvalues; // Using an array to store height values for the wave
 let colorUp=true;
 
@@ -57,11 +71,11 @@ let colorUp=true;
 function calcWave() {
   // Increment theta (try different values for
   // 'angular velocity' here)
-  theta += 0.02;
+  theta += 1;
 
   // For every x value, calculate a y value with sine function
   let x = theta;
-  for (let i = 0; i < yvalues.length; i++) {
+  for (let i = 1; i < yvalues.length; i++) {
     yvalues[i] = sketch.sin(x) * amplitude;
     x += dx;
   }
@@ -69,25 +83,53 @@ function calcWave() {
 
 function renderWave() {
   sketch.noStroke();
-    if (count == 275 && colorUp){
-      colorUp=false;
-    } 
-    if (count == 0 && !colorUp){
-        colorUp=true;
-    } 
-    if (colorUp) {
-        count+=1;
-    } else {
-        count-=1;
+    // if (count == 255 && colorUp){
+    //   colorUp=false;
+    // } 
+    // if (count == 0 && !colorUp){
+    //     colorUp=true;
+    // } 
+    // if (colorUp) {
+    //     count+=1;
+    // } else {
+    //     count-=1;
+    // }
+  if (honeyToRose) {
+    gradcount += incr;
+    fillcolor = sketch.lerpColor(honeydew, rosemist, gradcount);
+    if (gradcount > 1) {
+      honeyToRose = false;
+      roseToSeashell = true;
+      gradcount = 0;
     }
-  sketch.fill(count + 0, count + 0, count + 0);
+  }
+  if (roseToSeashell) {
+    gradcount += incr;
+    fillcolor = sketch.lerpColor(rosemist, seashell, gradcount);
+    if (gradcount > 1) {
+      roseToSeashell = false;
+      seashellToHoney = true;
+      gradcount = 0;
+    }
+  }
+  if (seashellToHoney) {
+    gradcount += incr;
+    fillcolor = sketch.lerpColor(seashell, honeydew, gradcount);
+    if (gradcount > 1) {
+      seashellToHoney = false;
+      honeyToRose = true;
+      gradcount = 0;
+    }
+  }
+  sketch.fill(fillcolor);
+  //sketch.fill(count + 1, count + 1, count + 1);
   // A simple way to draw the wave with an ellipse at each location
   for (let x = 0; x < yvalues.length; x++) {
-    sketch.ellipse(x * xspacing, sketch.height / 2 + yvalues[x], 16, 16);
+    sketch.ellipse(x * xspacing, sketch.height / 2 + yvalues[x], 26, 16);
   }
 }
 
 };
 
 let myp5 = new p5(s, 'dots');
-let myp52 = new p5(s, 'content');
+// let myp52 = new p5(s, 'content');
